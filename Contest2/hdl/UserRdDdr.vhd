01c9402b9a4f81ed667474e0a4957628438baac1
Natchapon Changbu <tonzazil12@gmail.com>
2021-12-25 00:26:34 +0700

    Init from another project

--- Contest2/hdl/UserRdDdr.vhd
+++ Contest2/hdl/UserRdDdr.vhd
@@ -25,7 +25,7 @@ Entity UserRdDdr Is
 	(
 		RstB			: in	std_logic;							-- use push button Key0 (active low)
 		Clk				: in	std_logic;							-- clock input 100 MHz
-		
+
 		DipSwitch		: in 	std_logic_vector( 1 downto 0 );
 		
 		-- HDMICtrl I/F
@@ -60,13 +60,35 @@ Architecture rtl Of UserRdDdr Is
 ----------------------------------------------------------------------------------
 -- Signal declaration
 ----------------------------------------------------------------------------------
+	
 	signal	rMemInitDone	: std_logic_vector( 1 downto 0 );
+	signal	rHDMIReq		: std_logic;
+
+	signal	rMtDdrRdReq		: std_logic;
+	signal	rMtDdrRdAddr	: std_logic_vector(28 downto 7);
+
+	type UserRdStateType is
+		(
+			stInit		,
+			stReq		,
+			stWtMtDone		
+		);
+	signal	rState			: UserRdStateType;
 	
 Begin
 
 ----------------------------------------------------------------------------------
 -- Output assignment
 ----------------------------------------------------------------------------------
+
+	HDMIReq			<= rHDMIReq;
+
+	MtDdrRdReq		<= rMtDdrRdReq;
+	MtDdrRdAddr(28 downto 7)		<= rMtDdrRdAddr(28 downto 7); 
+
+	URd2HFfWrEn	<=	D2URdFfWrEn;
+	URd2HFfWrData(63 downto 0)	<=	D2URdFfWrData(63 downto 0);
+	D2URdFfWrCnt(15 downto 0)	<=	URd2HFfWrCnt(15 downto 0);
 
 	
 ----------------------------------------------------------------------------------
@@ -84,5 +106,95 @@ Begin
 			end if;
 		end if;
 	End Process u_rMemInitDone;
+
+	u_rHDMIReq : Process (Clk) Is
+	Begin
+		if ( rising_edge(Clk) ) then
+			if ( RstB='0' ) then
+				rHDMIReq	<= '0';
+			else
+				if ( HDMIBusy='0' and rMemInitDone(1)='1' ) then
+					rHDMIReq	<= '1';
+				elsif ( HDMIBusy='1' )  then
+					rHDMIReq	<= '0';
+				else
+					rHDMIReq	<= rHDMIReq;
+				end if;
+			end if;
+		end if;
+	End Process u_rHDMIReq;
+
+	u_rMtDdrRdReq: process(Clk)
+	begin
+		if rising_edge(Clk) then
+			if RstB = '0' then
+				rMtDdrRdReq	<=	'0';
+			else
+				if (rState = stReq) then
+					rMtDdrRdReq	<=	'1';
+				else
+					rMtDdrRdReq	<=	'0';
+				end if ;
+			end if;
+		end if;
+	end process u_rMtDdrRdReq;
+
+	u_rMtDdrRdAddr: process(Clk)
+	begin
+		if rising_edge(Clk) then
+			if RstB = '0' then
+				rMtDdrRdAddr(28 downto 27)	<= DipSwitch(1 downto 0);
+				rMtDdrRdAddr(26 downto 7)	<=	(others => '0');
+			else
+				if( (rState = stWtMtDone) and (MtDdrRdBusy = '0') ) then
+					if rMtDdrRdAddr(21 downto 7) = ("101" & x"FFF") then
+						rMtDdrRdAddr(28 downto 27)	<= DipSwitch(1 downto 0);
+						rMtDdrRdAddr(26 downto 7)	<= (others => '0');
+					else
+						rMtDdrRdAddr(28 downto 7)	<= rMtDdrRdAddr(28 downto 7) + 1;
+					end if ;
+				else
+					rMtDdrRdAddr(28 downto 7)	<= rMtDdrRdAddr(28 downto 7);
+				end if;
+			end if;
+		end if;
+	end process u_rMtDdrRdAddr;
+
+----------------------------------------------------------------------------------
+-- State Machine 
+----------------------------------------------------------------------------------
+	u_rState: process(Clk)
+	begin
+		if rising_edge(Clk) then
+			if RstB = '0' then
+				rState	<=	stInit;
+			else
+				case( rState ) is
+
+					when stInit	=>
+						if ( rMemInitDone(1) = '1' ) then
+							rState	<= stReq;
+						else
+							rState	<= stInit;
+						end if ;
+
+					when stReq	=>
+						if ( MtDdrRdBusy = '1' ) then
+							rState 	<=	stWtMtDone;
+						else
+							rState 	<=	stReq;
+						end if ;
+
+					when stWtMtDone =>
+						if ( MtDdrRdBusy = '0' ) then
+							rState 	<=	stReq;
+						else
+							rState 	<=	stWtMtDone;
+						end if;
+				
+				end case ;
+			end if;
+		end if;
+	end process u_rState;
 	
 End Architecture rtl;
--- Contest2/hdl/UserWrDdr.vhd
+++ Contest2/hdl/UserWrDdr.vhd
@@ -25,7 +25,7 @@ Entity UserWrDdr Is
 	(
 		RstB			: in	std_logic;							-- use push button Key0 (active low)
 		Clk				: in	std_logic;							-- clock input 100 MHz
-		
+
 		-- WrCtrl I/F
 		MemInitDone		: in	std_logic;
 		MtDdrWrReq		: out	std_logic;
@@ -54,7 +54,20 @@ Architecture rtl Of UserWrDdr Is
 ----------------------------------------------------------------------------------
 -- Signal declaration
 ----------------------------------------------------------------------------------
+	
 	signal	rMemInitDone	: std_logic_vector( 1 downto 0 );
+
+	signal	rMtDdrWrReq		: std_logic;
+	signal	rMtDdrWrAddr	: std_logic_vector(28 downto 7);
+
+	type	UserWrStateType	is
+		(
+			stInit		,
+			stCheckFf	,
+			stReq		,
+			stWtMtDone	
+		);
+	signal	rState			: UserWrStateType;
 	
 Begin
 
@@ -62,7 +75,14 @@ Begin
 -- Output assignment
 ----------------------------------------------------------------------------------
 
+	--Bypass
+	T2UWrFfRdEn		<=	UWr2DFfRdEn;
+	UWr2DFfRdData( 63 downto 0 )	<=	T2UWrFfRdData( 63 downto 0 );
+	UWr2DFfRdCnt( 15 downto 0 )		<=	T2UWrFfRdCnt( 15 downto 0 );
 	
+	MtDdrWrReq		<=	rMtDdrWrReq;
+	MtDdrWrAddr(28 downto 7)		<=	rMtDdrWrAddr(28 downto 7);
+
 ----------------------------------------------------------------------------------
 -- DFF 
 ----------------------------------------------------------------------------------
@@ -78,5 +98,85 @@ Begin
 			end if;
 		end if;
 	End Process u_rMemInitDone;
+
+	u_rMtDdrWrReq: process(Clk)
+	begin
+		if rising_edge(Clk) then
+			if RstB = '0' then
+				rMtDdrWrReq		<=	'0';
+			else
+				if rState = stReq then
+					rMtDdrWrReq		<=	'1';
+				else
+					rMtDdrWrReq		<=	'0';
+				end if ;
+			end if;
+		end if;
+	end process u_rMtDdrWrReq;
+
+	u_rMtDdrWrAddr: process(Clk)
+	begin
+		if rising_edge(Clk) then
+			if RstB = '0' then
+				rMtDdrWrAddr(28 downto 7)	<=	(others => '0');
+			else
+				if( (rState = stWtMtDone) and (MtDdrWrBusy = '0') ) then
+					if rMtDdrWrAddr(21 downto 7) = ("101" & x"FFF") then
+						rMtDdrWrAddr(28 downto 27)	<= rMtDdrWrAddr(28 downto 27) + 1;
+						rMtDdrWrAddr(26 downto 7)	<= (others => '0');
+					else
+						rMtDdrWrAddr(28 downto 7)	<= rMtDdrWrAddr(28 downto 7) + 1;
+					end if ;
+				else
+					rMtDdrWrAddr(28 downto 7)	<= rMtDdrWrAddr(28 downto 7);
+				end if;
+			end if;
+		end if;
+	end process u_rMtDdrWrAddr;
+
+----------------------------------------------------------------------------------
+-- State Machine 
+----------------------------------------------------------------------------------
 	
+	u_rState: process(Clk)
+	begin
+		if rising_edge(Clk) then
+			if RstB = '0' then
+				rState	<=	stInit;
+			else
+				case( rState ) is
+				
+					when stInit =>
+						if rMemInitDone(1) = '1' then
+							rState	<=	stCheckFf;
+						else
+							rState	<=	stInit;
+						end if ;	
+					
+					when stCheckFf	=>
+						if T2UWrFfRdCnt( 15 downto 4 ) /= 0 then
+							rState	<=	stReq;
+						else
+							rState	<=	stCheckFf;
+						end if ;
+
+					when stReq	=>
+						if MtDdrWrBusy = '1' then
+							rState	<=	stWtMtDone;
+						else
+							rState	<=	stReq;
+						end if ;
+
+					when stWtMtDone	=>
+						if MtDdrWrBusy = '0' then
+							rState	<=	stCheckFf;
+						else
+							rState	<=	stWtMtDone;
+						end if ;
+				
+				end case ;
+			end if;
+		end if;
+	end process u_rState;
+
 End Architecture rtl;
